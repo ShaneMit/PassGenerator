2c0601447f7b6803a40d4441b9ab56a3199a55f6
ShaneMit <shanemit@cox.net>
2020-04-23 20:47:06 -0700

    added lets

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add variables
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let numbers = '123456789'
+let special = '!$*&@'
+let passwordL = parseInt(prompt('What is your desired password length?'))
+let low = confirm('Would you like to use lower case letters?')
+let up = confirm('Would you like to use upper case letters?')
+let numbersC = confirm('Would like to use numbers?')
+let symbols = confirm(' Would you like to use special characters?')
+
+
+
+
