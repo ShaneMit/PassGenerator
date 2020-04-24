6d11f483df539934bc3c39cd8ee1a9542adef862
ShaneMit <shanemit@cox.net>
2020-04-23 23:46:50 -0700

    Tryed things that didn't work

--- Develop/script.js
+++ Develop/script.js
@@ -13,21 +13,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+  
 // Variables
+
 let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
 let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 let numbers = '123456789'
 let special = '!$*&@'
-let length = parseInt(prompt('What is your desired password length?'))
+let passLength = parseInt(prompt('What is your desired password length?'))
 let low = confirm('Would you like to use lower case letters?')
 let up = confirm('Would you like to use upper case letters?')
 let numbersC = confirm('Would like to use numbers?')
 let symbols = confirm(' Would you like to use special characters?')
-let passHolder = ''
-let newPassword = ''
+let passHolder = ' '
+let newPassword = ' '
 
-
-if (length < 8 || length > 128) {
+// Conditional
+if (passLength < 8 || passLength > 128) {
   alert('Password must be between 8 and 128 characters long');
 }
 if (low) {
@@ -40,8 +42,18 @@ if (numbersC) {
   passHolder += numbers;
 }
 if (symbols) {
-  passholder += special;
+  passHolder += special;
 }
+
+for(let i = 0; i < passLength; i++) {
+  let randomIndex = Math.floor(Math.random() * passHolder.length)
+  newPassword += passHolder[randomIndex]
+}
+
+
+
+
+
 
 
 
