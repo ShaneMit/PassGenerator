dcae2287d53ed73dba6bd9b8a80db6abd2818996
ShaneMit <shanemit@cox.net>
2020-04-26 14:04:24 -0700

    Ordering, missing curly brace, added comments

--- Develop/script.js
+++ Develop/script.js
@@ -2,23 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
-  const passLength = parseInt(prompt('What is your desired password length?'))
-  if (passLength < 8 || passLength > 128) {
-    alert('Password must be between 8 and 128 characters long');
+
+// Length prompt
+const passLength = parseInt(prompt('What is your desired password length?'))
+ if (passLength < 8 || passLength > 128) {
+    alert ('Password must be between 8 and 128 characters long');
+    return
 }
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
   
 // Variables
 
@@ -30,11 +20,10 @@ const low = confirm('Would you like to use lower case letters?')
 const up = confirm('Would you like to use upper case letters?')
 const numbersC = confirm('Would like to use numbers?')
 const symbols = confirm(' Would you like to use special characters?')
-let passHolder = ' '
-let newPassword = ' '
+let passHolder = ''
+let newPassword = ''
 
 // Conditional
-
 
 if (low) {
   passHolder += lowerCase;
@@ -49,12 +38,24 @@ if (symbols) {
   passHolder += special;
 }
 
+// Random character selector
 for(let i = 0; i < passLength; i++) {
   let randomIndex = Math.floor(Math.random() * passHolder.length)
   newPassword += passHolder[randomIndex]
 }
+return newPassword
+}
 
+function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
 
+   passwordText.value = password;
+
+  }
+
+// Make button work
+  generateBtn.addEventListener("click", writePassword);
 
 
 
